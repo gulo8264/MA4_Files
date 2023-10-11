e5b6484cc38bbb8a63a7862a0cceecc3061f13cf
gulo8264 <gustaf.lofdahl@hotmail.com>
2023-10-11 20:15:57 +0200

    DONE

--- MA4_2.py
+++ MA4_2.py
@@ -13,18 +13,18 @@ from time import perf_counter as pc
 #Numba
 from numba import njit
 
-def fib_py(n):
+def fib_p(n):
     if n <= 1:
         return n
     else:
-        return(fib_py(n-1) + fib_py(n-2))
+        return(fib_p(n-1) + fib_p(n-2))
     
 @njit
-def fib_numba(n):
+def fib_n(n):
     if n<= 1:
         return n
     else:
-        return fib_numba(n-1) + fib_numba(n-2)
+        return fib_n(n-1) + fib_n(n-2)
 
 def comp(q,w):
     x = 0
@@ -39,9 +39,9 @@ def comp(q,w):
         f = Person(i)
         f.fib()
         t2 = pc()
-        fib_numba(i)
+        fib_n(i)
         t3 = pc()
-        fib_py(i)
+        fib_p(i)
         t4 = pc()
         y_c.append(t2-t1)
         y_n.append(t3-t2)
@@ -55,20 +55,20 @@ def comp(q,w):
     plt.title('time comparison')
     plt.savefig(str(q) + "to" + str(w) + 'log.png')
 
-def time47():
-    x = 47
+def time(x):
     t1 = pc()
     f = Person(x)
     print(f'C++: {f.fib()}')
     t2 = pc()
-    print(f'numba: {fib_numba(x)}')
+    print(f'Numba: {fib_n(x)}')
     t3 = pc()
-    print(f'C++: {round(t2-t1,4)}')
-    print(f'Numba: {round(t3-t2,4)}')
+    print(f'C++: {round(t2-t1,4)}s')
+    print(f'Numba: {round(t3-t2,4)}s')
 
 def main():
     main_start = pc()
-    #time47()
+    #time(47)
+    comp(20,30)
     comp(30,45)
     main_end = pc()
     print('Done')
